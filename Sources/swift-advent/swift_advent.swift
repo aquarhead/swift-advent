bc18379dcdd803b12934c3c0ccf7ad92304ec999
LOU Xun <aquarhead@gmail.com>
2022-12-07 19:38:54 +0000

    up to day7

--- Sources/swift-advent/swift_advent.swift
+++ Sources/swift-advent/swift_advent.swift
@@ -3,6 +3,6 @@ public struct swift_advent {
     public static func main() {
         // let path = Bundle.main.path(forResource: "day1", ofType: "txt")
         // let input = try! String(contentsOfFile: path!)
-        day4.run()
+        day7.run()
     }
 }
